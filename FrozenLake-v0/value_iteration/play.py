c2c9c565e70db90c6fd68b1269ca2f129fa3ce8e
mrtovsky <emerte.quant@gmail.com>
2020-01-25 20:48:13 +0100

    Implement methods for action selection

--- FrozenLake-v0/value_iteration/play.py
+++ FrozenLake-v0/value_iteration/play.py
@@ -20,9 +20,37 @@ class Agent(object):
         self.state = self.env.reset()
 
     def play_random_steps(self, n_steps):
-        action = self.env.action_space.sample()
-        new_state, reward, is_done, _ = self.env.step(action)
+        for _ in range(n_steps):
+            action = self.env.action_space.sample()
+            new_state, reward, is_done, _ = self.env.step(action)
 
-        self.rewards[new_state] = reward
-        self.transitions[(self.state, action)][new_state] += 1
-        self.state = self.env.reset() if is_done else new_state
+            if new_state not in self.rewards.keys():
+                self.rewards[new_state] = reward
+            self.transitions[(self.state, action)][new_state] += 1
+            self.state = self.env.reset() if is_done else new_state
+
+    def calculate_action_value(self, state, action):
+        transition_counts = self.transitions[(state, action)]
+        n_transitions = sum(transition_counts.values())
+
+        action_value = 0
+        for new_state, count in transition_counts.items():
+            reward = self.rewards[new_state]
+            proba_new_state = count / n_transitions
+            action_value += (
+                proba_new_state
+                * (reward + GAMMA * self.values[new_state])
+            )
+
+        return action_value
+
+    def pick_best_action(self, state):
+        best_action, best_value = None, 0
+
+        for action in range(self.env.action.n):
+            action_value = self.calculate_action_value(state, action)
+            if best_value < action_value:
+                best_value = action_value
+                best_action = action
+
+        return best_action
