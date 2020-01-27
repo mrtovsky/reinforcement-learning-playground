ef241f0e6f57b874ece5bf2c6201e52dba362d9c
mrtovsky <emerte.quant@gmail.com>
2020-01-27 23:59:03 +0100

    Create complete game solving agent
    Implement value iteration method and environment training execution.

--- FrozenLake-v0/value_iteration/play.py
+++ FrozenLake-v0/value_iteration/play.py
@@ -8,6 +8,7 @@ from torch.utils.tensorboard import SummaryWriter
 ENV_NAME = "FrozenLake-v0"
 GAMMA = .9
 TEST_EPISODES = 20
+REWARD_THRESHOLD = .8
 
 
 class Agent(object):
@@ -34,6 +35,7 @@ class Agent(object):
         n_transitions = sum(transition_counts.values())
 
         action_value = 0
+
         for new_state, count in transition_counts.items():
             reward = self.rewards[new_state]
             proba_new_state = count / n_transitions
@@ -45,12 +47,70 @@ class Agent(object):
         return action_value
 
     def pick_best_action(self, state):
-        best_action, best_value = None, 0
-
-        for action in range(self.env.action.n):
+        for idx, action in enumerate(range(self.env.action_space.n)):
             action_value = self.calculate_action_value(state, action)
-            if best_value < action_value:
+            if idx == 0:
+                best_action = action
+                best_value = action_value
+            elif best_value < action_value:
                 best_value = action_value
                 best_action = action
 
         return best_action
+
+    def play_episode(self, env):
+        state = env.reset()
+        total_reward = 0
+
+        is_done = False
+
+        while not is_done:
+            action = self.pick_best_action(state)
+            new_state, reward, is_done, _ = env.step(action)
+
+            if new_state not in self.rewards.keys():
+                self.rewards[new_state] = reward
+            self.transitions[(state, action)][new_state] += 1
+            total_reward += reward
+            state = new_state
+
+        return total_reward
+
+    def iterate_values(self):
+        for state in range(self.env.observation_space.n):
+            state_values = [
+                self.calculate_action_value(state, action)
+                for action in range(self.env.action_space.n)
+            ]
+            self.values[state] = max(state_values)
+
+
+if __name__ == "__main__":
+    env_test = gym.make(ENV_NAME)
+    agent = Agent()
+    writer = SummaryWriter(comment="-value-iteration")
+
+    best_reward = 0
+    well_trained = False
+    idx = 0
+
+    while not well_trained:
+        idx += 1
+
+        agent.play_random_steps(100)
+        agent.iterate_values()
+
+        reward = 0
+        for _ in range(TEST_EPISODES):
+            reward += agent.play_episode(env_test)
+        reward /= TEST_EPISODES
+
+        writer.add_scalar("reward", reward, idx)
+
+        if reward > best_reward:
+            best_reward = reward
+
+        if reward > REWARD_THRESHOLD:
+            print("Solved in {} nr of steps!".format(idx))
+            well_trained = True
+    writer.close()
